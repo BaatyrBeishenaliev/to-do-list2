9c9ff378f923e26d222f0ead51d5956b1198c9d4
BatByGeek <90051654+BaatyrBeishenaliev@users.noreply.github.com>
2023-02-05 15:53:17 +0600

    Update script.js

--- to do list/script/script.js
+++ to do list/script/script.js
@@ -1,21 +1,40 @@
 const input = document.querySelector('#input')
 const button = document.querySelector('#button')
-const innerBLock = document.querySelector('.inner-block')
-const tasks = document.querySelector('.tasks');
-const button2 = document.querySelector('.button');
+const outputBlock = document.querySelector('.outputBlock')
+const left = document.querySelector('.left')
+const right = document.querySelector('.right')
+
 
 button.addEventListener('click', function (event) {
     event.preventDefault()
 
+    if (input.value === "") {
+        alert('Заполните все поля!')
+        p.remove()
+        doneBtn.remove()
+    }
+
+    const div = document.createElement('div')
+    div.classList.add('inner-block')
+    outputBlock.append(div)
+
     const p = document.createElement('p')
     p.classList.add('task')
-    p.textContent = input.value
+    p.innerHTML = input.value
 
     const doneBtn = document.createElement('button')
     doneBtn.classList.add('doneBtn')
-    doneBtn.textContent = 'done'
+    doneBtn.innerHTML = 'done'
 
+    div.append(left)
+    div.append(right)
+    left.append(p)
+    right.append(doneBtn)
 
-    tasks.append(p)
-    button2.append(doneBtn)
+    doneBtn.addEventListener('click', function (event) {
+        event.preventDefault()
+        p.style.textDecoration = 'line-through'
+        doneBtn.innerHTML = 'done ✅'
+        doneBtn.style.background = 'grey'
+    })
 })
